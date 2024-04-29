68621f45dadde0984b67156f3a62b2f90d2a6643
vipulSharma18 <vipuls181999@gmail.com>
2024-04-28 20:44:13 -0400

    binning and spuriosity val per class calc done for new data loader and oscar data

--- rank_calculation/rank_data.py
+++ rank_calculation/rank_data.py
@@ -11,42 +11,32 @@ sys.path.append(parent_dir)
 
 from compute_feature_activations import * 
 from spuriosity_metrics import * 
+from configs.config import Config
 
 if __name__ == '__main__': 
-    #get the feature activations
-    model_path = '../models/robust_resnet50.pth'
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    architecture = 'resnet50'
-    encoder = get_encoder(model_path, device, architecture)
-    #setup_data_loaders(split='val', shuffle=False, bin=None, rank_calculation=False):
-    train = setup_data_loaders()
-    valid = setup_data_loaders()
-    cache_fname_train = '../cached_feature_activations_train'
-    cache_fname_valid = '../cached_feature_activations_valid'
-    print("Calculating training images feature activations")
-    train_ftrs, train_labels = calculate_feature_activations(encoder, train, cache_fname_train, device)
-    print("Calculating validation images feature activations")
-    valid_ftrs, valid_labels = calculate_feature_activations(encoder, valid, cache_fname_valid, device)
-    print('train:',train_ftrs.shape, train_labels.shape)
-    print('valid:',valid_ftrs.shape, valid_labels.shape)
-    
+    config = Config()
 
     #get the spurious features by class
-    aggregated_data_path = 'data_annotations/spurious_imagenet_classes.csv'
+    aggregated_data_path = '../data_annotations/spurious_imagenet_classes.csv'
     aggregated_human_labels = pd.read_csv(aggregated_data_path)
     spurious_features_by_class = calc_spurious_features_by_class(aggregated_human_labels)
-    print([spurious_features_by_class[i] for i in list(spurious_features_by_class.keys())[:5]])
     print('output check', len(spurious_features_by_class), ', should be 357 for imagenet.')
 
-    #train spuriosity calculation 
-    #spuriosity_data_train = calculate_spuriosity_per_class(train_ftrs, train_labels, spurious_features_by_class)
-    #binned_img_idx_train = bin_by_spuriosity(spuriosity_data_train, train_labels, spurious_features_by_class)
-    np.save('../data_annotations/train_spuriosity_per_image.npy', spuriosity_data_train)
-    np.save('../data_annotations/train_binned_image_indices.npy', binned_img_idx_train)
-    
     #validation spuriosity calcuation
-    spuriosity_data_valid = calculate_spuriosity_per_class(valid_ftrs, valid_labels, spurious_features_by_class)
-    binned_img_idx_valid = bin_by_spuriosity(spuriosity_data_valid, valid_labels, spurious_features_by_class)
-    np.save('../data_annotations/valid_spuriosity_per_image.npy', spuriosity_data_valid)
-    np.save('../data_annotations/valid_binned_image_indices.npy', binned_img_idx_valid)
-
+    val_raw_feature_act_path = '../feature_activations_data/robust_resnet_50_imagenet_complete/feature_activations_valid_resnet50_.csv'
+    val_spuriosity_path = '../data_annotations/validation_imagenet_spuriosity.csv'
+    rows_processed = calculate_spuriosity_per_class(val_raw_feature_act_path, val_spuriosity_path, spurious_features_by_class)
+    print("Generated spuriosity for validation data, rows produced", rows_processed)
+    if rows_processed: 
+        binned_img_idx_valid = bin_by_spuriosity(val_spuriosity_path, config.bin_file_path_val, spurious_features_by_class)
+        print("Processed binning of", binned_img_idx_valid, "rows of validation spuriosity data.")
+    
+    #train spuriosity calcuation
+    train_raw_feature_act_path = '../feature_activations_data/robust_resnet_50_imagenet_complete/feature_activations_train_resnet50_.csv'
+    train_spuriosity_path = '../data_annotations/train_imagenet_spuriosity.csv'
+    rows_processed = calculate_spuriosity_per_class(train_raw_feature_act_path, train_spuriosity_path, spurious_features_by_class)
+    print("Generated spuriosity for train data, rows produced", rows_processed)
+    if rows_processed: 
+        binned_img_idx_train = bin_by_spuriosity(train_spuriosity_path, config.bin_file_path_train, spurious_features_by_class)
+        print("Processed binning of", binned_img_idx_train, "rows of train spuriosity data.")
+    
--- rank_calculation/spuriosity_metrics.py
+++ rank_calculation/spuriosity_metrics.py
@@ -21,7 +21,6 @@ import sys
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
 
-
 def calc_spurious_features_by_class(aggregated_human_labels): 
     '''
     aggregated_human_labels is the csv file spurious_imagenet.csv in data_annotations. 
@@ -31,60 +30,64 @@ def calc_spurious_features_by_class(aggregated_human_labels):
     class_ftr_idx = dict(class_ftr_idx)
     return class_ftr_idx
 
-def calculate_spuriosity_per_class(feature_activations, class_labels, spurious_features_by_class):
+def count_rows(filename):
+    with open(filename, 'r') as file:
+        num_rows = sum(1 for line in file)
+    return num_rows
+
+def cache_data(cache_path, data_to_cache):
     '''
-    inputs: 
-    feature_activations: a tensor/array of num_of_images, 2048 feature activations of resnet for imagenet trained resnet
-    class_labels: a tensor/array of num_of_images, class label (0-999)
+    schema of output cache: 
+    Input.class_index,image_name,spuriosity
+    '''
+    data_to_cache.to_csv(cache_path, mode='a', header=not os.path.exists(cache_path), index=False)
+
+def calculate_spuriosity_per_class(ftr_activations_path, output_path, spurious_features_by_class):
+    '''
     spurious_features_by_class: a dictionary of class label (0-999) and spurious feature index (0-2047)    
-    
-    output: 
-    spuriosity_values_of_data: a tensor/array of num_of_images, 1 (spuriosity value within class). Same order as class_labels.
     '''
-    spuriosity_values_of_data = np.zeros((len(class_labels), 1))
+    feature_activations = pd.read_csv(ftr_activations_path)
     
     for class_idx in spurious_features_by_class: 
-        spurious_features_idx = spurious_features_by_class[class_idx]
-        image_idx_of_class = np.where(class_labels == class_idx)[0]
-        
-        feature_activations_of_class = feature_activations[image_idx_of_class]
-        mean_feature_activations_of_class = np.mean(feature_activations_of_class, axis=0)
-        std_feature_activations_of_class = np.std(feature_activations_of_class, axis=0)
-        z_score_feature_activations_of_class = (feature_activations_of_class - mean_feature_activations_of_class)/std_feature_activations_of_class
-        
-        spuriosity_values_of_class = np.average(z_score_feature_activations_of_class[:, spurious_features_idx], axis=1)
-        spuriosity_values_of_class = np.reshape(spuriosity_values_of_class, (-1, 1))
+        spurious_features_idx = np.array(spurious_features_by_class[class_idx])
+        class_data = feature_activations[feature_activations['Input.class_index']==class_idx]
+        feature_activations_of_class = class_data.iloc[:, 2:].iloc[:,spurious_features_idx]
+        zscore_activations = (feature_activations_of_class - feature_activations_of_class.mean())/feature_activations_of_class.std()
+        spuriosity = zscore_activations.mean(axis=1)
+        spuriosity_df = pd.DataFrame({
+            'Input.class_index': class_data['Input.class_index'],
+            'image_name': class_data['Image.file_name'],
+            'spuriosity': spuriosity
+        })
+        cache_data(output_path, spuriosity_df)
+        del spuriosity_df, spuriosity, zscore_activations
+    return count_rows(output_path)
 
-        spuriosity_values_of_data[image_idx_of_class] = spuriosity_values_of_class
-        
-    return spuriosity_values_of_data
-
-def bin_by_spuriosity(spuriosity_values_of_data, class_labels, spurious_features_by_class): 
+def bin_by_spuriosity(spuriosity_path, output_path, spurious_features_by_class): 
     '''
     bin the data per class by 0-25 percentile as low spurious, 25-75 as medium 
     and 75-100 percentile spuriosity as high spurious
-    
     spurious_features_by_class: dict, key class, value spurious feature index
-    class_labels: array num_images, (0-999) class idx
-    spuriosity_values_of_data: array num_images, spuriosity value as calculated within class
-    
-    output: dict with spurious feature containing class as key and array value (3, idx of images of a class in original data)
     '''
-    result = {}
+    wordnet_data = pd.read_csv('../data_annotations/imagenet_class_metadata.csv')
+    sp_vals_data = pd.read_csv(spuriosity_path)
+    sp_vals_data.merge(wordnet_data[['Input.class_index', 'Input.wordnet_id']], how='left', on='Input.class_index')
     for class_idx in spurious_features_by_class: 
-        image_idx_of_class = np.where(class_labels == class_idx)[0]
-        spurious_vals_class = spuriosity_values_of_data[image_idx_of_class]
-        percentiles = np.percentile(spurious_vals_class, [25,75])
-        low_bin = image_idx_of_class[np.where(spurious_vals_class<=percentiles[0])]
-        mid_bin = image_idx_of_class[np.where((spurious_vals_class>percentiles[0]) & (spurious_vals_class<=percentiles[1]))]
-        high_bin = image_idx_of_class[np.where(spurious_vals_class>percentiles[1])]
-        result[class_idx] = np.array([low_bin, mid_bin, high_bin])
-    return result
+        spuriosity_val_class = sp_vals_data[sp_vals_data['Input.class_index'] == class_idx]
+        percentiles = np.percentile(spuriosity_val_class['spuriosity'], [25, 75]) # percentils[0] 25%, percentiles[1] 75%
+        result_df = spuriosity_val_class[['Input.wordnet_id', 'Input.class_index', 'image_name']]
+        result_df['bin_type'] = spuriosity_val_class['spuriosity'].apply(lambda x: 0 if x<=percentiles[0] else x)
+        result_df['bin_type'] = spuriosity_val_class['spuriosity'].apply(lambda x: 1 if (x>percentiles[0] & x<=percentiles[1]) else x)
+        result_df['bin_type'] = spuriosity_val_class['spuriosity'].apply(lambda x: 2 if x>percentiles[1] else x)
+        
+        cache_data(output_path, result_df)
+        del result_df
+    return count_rows(output_path)
 
 if __name__ == '__main__': 
-    #the file paths work if you run from root and not within the module folder
-    aggregated_data_path = 'data_annotations/spurious_imagenet_classes.csv'
+    aggregated_data_path = '../data_annotations/spurious_imagenet_classes.csv'
     aggregated_human_labels = pd.read_csv(aggregated_data_path)
     spurious_features_by_class = calc_spurious_features_by_class(aggregated_human_labels)
+    #sanity check: for 5 random classes, get the spurious feature indices
     print([spurious_features_by_class[i] for i in list(spurious_features_by_class.keys())[:5]])
     print('output check', len(spurious_features_by_class), ', should be 357 for imagenet.')

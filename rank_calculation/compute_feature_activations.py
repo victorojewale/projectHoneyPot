edited code to use normalization from transform rather than model layers for binning and ftr activation logic bugfix

rank_calculation/compute_feature_activations.py
@@ -74,7 +74,7 @@ def calculate_feature_activations(encoder, loader, cache_fname, device='cuda'):
         batch_num = 0
         #for train loader, the shuffle needs to be set to false
         for dat in loader:
-            x, y, fname = dat[0].to(device), dat[1], dat[2]
+            x, y, fname = dat['image'].to(device), dat['label'], dat['file_name']
             with torch.no_grad():
                 ftrs = encoder(x.to(device)).flatten(1)
             ftrs = np.array(ftrs.detach().cpu().numpy())
0566efe21ae6d47b33b8ba8e80850b0903ecfd4e
vipul <vipul_sharma@brown.edu>
2024-05-24 14:43:14 -0400

    change rank data to run from root. cleaned data annotations. removed old spuriosity and bin data to allow new ones to be created

--- rank_calculation/rank_data.py
+++ rank_calculation/rank_data.py
@@ -17,21 +17,23 @@ if __name__ == '__main__':
     config = Config()
 
     #get the spurious features by class
-    aggregated_data_path = '../data_annotations/spurious_imagenet_classes.csv'
+    aggregated_data_path = './data_annotations/spurious_imagenet_classes.csv'
     aggregated_human_labels = pd.read_csv(aggregated_data_path)
     spurious_features_by_class = calc_spurious_features_by_class(aggregated_human_labels)
     print('output check', len(spurious_features_by_class), ', should be 357 for imagenet.')
 
     #Use below version of the code if given is the spuriosity values and you just need to create new binning categories by diff binning logic
+    '''
     train_spuriosity_path = '../data_annotations/train_imagenet_spuriosity.csv'
     binned_img_idx_train = bin_by_spuriosity(train_spuriosity_path, config.bin_file_path_train, spurious_features_by_class)
     print("Processed binning of", binned_img_idx_train, "rows of train spuriosity data.")
+    '''
     
     #Use below below version of the code if given is feature activations and you want to calculate spuriosity and binning categories
-    '''
+    
     #validation spuriosity calcuation
-    val_raw_feature_act_path = '../feature_activations_data/robust_resnet_50_imagenet_complete/feature_activations_valid_resnet50_.csv'
-    val_spuriosity_path = '../data_annotations/validation_imagenet_spuriosity.csv'
+    val_raw_feature_act_path = './feature_activations_data/robust_resnet_50_imagenet_complete/feature_activations_valid_resnet50_.csv'
+    val_spuriosity_path = './data_annotations/validation_imagenet_spuriosity.csv'
     rows_processed = calculate_spuriosity_per_class(val_raw_feature_act_path, val_spuriosity_path, spurious_features_by_class)
     print("Generated spuriosity for validation data, rows produced", rows_processed)
     if rows_processed: 
@@ -39,11 +41,11 @@ if __name__ == '__main__':
         print("Processed binning of", binned_img_idx_valid, "rows of validation spuriosity data.")
     
     #train spuriosity calcuation
-    train_raw_feature_act_path = '../feature_activations_data/robust_resnet_50_imagenet_complete/feature_activations_train_resnet50_.csv'
-    train_spuriosity_path = '../data_annotations/train_imagenet_spuriosity.csv'
+    train_raw_feature_act_path = './feature_activations_data/robust_resnet_50_imagenet_complete/feature_activations_train_resnet50_.csv'
+    train_spuriosity_path = './data_annotations/train_imagenet_spuriosity.csv'
     rows_processed = calculate_spuriosity_per_class(train_raw_feature_act_path, train_spuriosity_path, spurious_features_by_class)
     print("Generated spuriosity for train data, rows produced", rows_processed)
     if rows_processed: 
         binned_img_idx_train = bin_by_spuriosity(train_spuriosity_path, config.bin_file_path_train, spurious_features_by_class)
         print("Processed binning of", binned_img_idx_train, "rows of train spuriosity data.")
-    '''
+    
